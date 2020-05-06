f5f8e91efaaf6614b2a3db6553a7f5a0a11da4da
danfordjooste <danfordjooste@sandiego.edu>
2020-05-05 19:44:30 -0700

    Add files via upload
    Added database config details

--- db_config.py
+++ db_config.py
@@ -3,9 +3,9 @@ from flaskext.mysql import MySQL
 
 mysql = MySQL()
  
-# MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Myrtle100!'
-app.config['MYSQL_DATABASE_DB'] = 'roytuts'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+# MySQL configurations (port = 3306)
+app.config['MYSQL_DATABASE_USER'] = 'sql3338132'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'HHE61CWk5S'
+app.config['MYSQL_DATABASE_DB'] = '	sql3338132'
+app.config['MYSQL_DATABASE_HOST'] = 'sql3.freemysqlhosting.net'
 mysql.init_app(app)
