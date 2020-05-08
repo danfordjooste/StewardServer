cdb3bbaad0b323bdbf1b055bdd84ed69aa8e018e
danfordjooste <danfordjooste@sandiego.edu>
2020-05-07 17:31:13 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,42 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask import request, redirect, render_template, url_for
 
 app = Flask(__name__)
-app.secret_key = "secret key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost/flasktut'
+#app.config['SECRET_KEY'] = 'super-secret'
+#app.config['SECURITY_REGISTERABLE'] = True
+app.debug = True
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+            self.username = username
+            self.email = email
+    
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+@app.route('/')
+def index():
+    myUser = User.query.all()
+    oneItem = User.query.filter_by(username="test2").all()
+    return render_template('add_user.html', myUser=myUser,oneItem=oneItem)
+
+@app.route('/post_user', methods=['POST'])
+def post_user():
+    user = User(request.form['username'], request.form['email'])
+    # this add to the database the user
+    db.session.add(user)
+    # this saves this data in the database
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+if __name__ == "__main__":
+    app.run()
