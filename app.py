ef98217deea31ba08ca37166132b78f47fd293b7
danfordjooste <danfordjooste@sandiego.edu>
2020-05-09 17:09:42 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
 from flask import Flask
+import datetime
 from flask_sqlalchemy import SQLAlchemy
-from flask import request, redirect, render_template, jsonify
+from flask import request, redirect, url_for, render_template, jsonify
 from flask_migrate import Migrate
 
 app = Flask(__name__)
@@ -9,7 +10,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     'DATABASE_URI',
     'postgresql://postgres:root@localhost/flasktut1')
-
+#app.debug=True
     
 #app.config['SECRET_KEY'] = 'super-secret'
 #app.config['SECURITY_REGISTERABLE'] = True
@@ -19,23 +20,165 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
-class User(db.Model):
+class pourDB(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=False)
-    email = db.Column(db.String(120), unique=False)
+    deviceNum = db.Column(db.String(20))
+    deviceName = db.Column(db.String(40))
+    pourVolume = db.Column(db.Integer)
+    approxTime = db.Column(db.String(20))
+    commitTime = db.Column(db.String(20))
+    battVolt = db.Column(db.Integer)
+    timerCounter = db.Column(db.Integer)
+    pourTime = db.Column(db.Integer)
+    pulseCount = db.Column(db.Integer)
 
-    def __init__(self, username, email):
-            self.username = username
-            self.email = email
+    def __init__(self, deviceNum, deviceName, pourVolume, approxTime, commitTime, battVolt, timerCounter, pourTime, pulseCount):
+            self.deviceNum = deviceNum
+            self.deviceName = deviceName
+            self.pourVolume = pourVolume
+            self.approxTime = approxTime
+            self.commitTime = commitTime
+            self.battVolt = battVolt
+            self.timerCounter = timerCounter
+            self.pourTime = pourTime
+            self.pulseCount = pulseCount
     
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<pourDB %r>' % self.deviceNum
+
+class deviceDB(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    deviceNum = db.Column(db.String(20))
+    deviceName = db.Column(db.String(40))
+    OnOff = db.Column(db.Integer)
+    last_battVolt = db.Column(db.Integer)
+    last_rcPeriod = db.Column(db.Integer)
+    last_update = db.Column(db.Integer)
+
+    def __init__(self, deviceNum, deviceName, OnOff, last_battVolt, last_rcPeriod, last_update):
+            self.deviceNum = deviceNum
+            self.deviceName = deviceName
+            self.OnOff = OnOff
+            self.last_battVolt = last_battVolt
+            self.last_rcPeriod = last_rcPeriod
+            self.last_update = last_update
+    
+    def __repr__(self):
+        return '<deviceDB %r>' % self.deviceNum
+
+class timeDB(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    mon = db.Column(db.Integer)
+    tue = db.Column(db.Integer)
+    wed = db.Column(db.Integer)
+    thu = db.Column(db.Integer)
+    fri = db.Column(db.Integer)
+    sat = db.Column(db.Integer)
+    sun = db.Column(db.Integer)
+
+    def __init__(self, mon, tue, wed, thu, fri, sat, sun):
+            self.mon = mon
+            self.tue = tue
+            self.wed = wed
+            self.thu = thu
+            self.fri = fri
+            self.sat = sat
+            self.sun = sun
+    
+    def __repr__(self):
+        return '<timeDB %r>' % self.mon
 
 @app.route('/')
 def index():
-    myUser = User.query.all()
-    oneItem = User.query.filter_by(username="test2").all()
-    return render_template('add_user.html', myUser=myUser,oneItem=oneItem)
+    dt = datetime.datetime.now()#gets local time
+    currentDay = dt.weekday()
+    currentTime = dt.strftime("%D %H:%M:%S")
+    myPour = pourDB.query.all()
+    myDevice = deviceDB.query.all()
+    onTime = timeDB.query.filter_by(id='1')
+    offTime = timeDB.query.filter_by(id='2')
+    return render_template('home.html', myPour=myPour, currentTime=currentTime, currentDay=currentDay, myDevice=myDevice, onTime=onTime,offTime=offTime)
+
+@app.route('/addOnTime')
+def addOnTime():
+    return render_template('newTime.html', title='Add ON Time:')
+
+@app.route('/addOnTimeDatabase',methods=['POST'])
+def addOnTimeDatabase():
+        onTime = timeDB(request.form['mon'],request.form['tue'],request.form['wed'],request.form['thu'],request.form['fri'],request.form['sat'],request.form['sun'])
+        db.session.add(onTime)
+        db.session.commit()    
+        return redirect(url_for('index'))
+
+@app.route('/editOnTime')
+def editOnTime():
+    return render_template('editOnTime.html',title='Edit ON Time:')
+
+@app.route('/editOnTimeDatabase',methods=['POST'])
+def editOnTimeDatabase():
+    editOn = timeDB.query.get({"id": 1})
+    editOn.mon = request.form['mon']
+    editOn.tue = request.form['tue']
+    editOn.wed = request.form['wed']
+    editOn.thu = request.form['thu']
+    editOn.fri = request.form['fri']
+    editOn.sat = request.form['sat']
+    editOn.sun = request.form['sun']
+    db.session.commit()  
+    return redirect(url_for('index'))
+
+@app.route('/editOffTime')
+def editOffTime():
+    return render_template('editOffTime.html',title='Edit OFF Time:')
+
+@app.route('/editOffTimeDatabase',methods=['POST'])
+def editOffTimeDatabase():
+    editOn = timeDB.query.get({"id": 2})
+    editOn.mon = request.form['mon']
+    editOn.tue = request.form['tue']
+    editOn.wed = request.form['wed']
+    editOn.thu = request.form['thu']
+    editOn.fri = request.form['fri']
+    editOn.sat = request.form['sat']
+    editOn.sun = request.form['sun']
+    db.session.commit()  
+    return redirect(url_for('index'))
+
+@app.route('/getTime', methods=['GET'])
+def getTime():
+    dt = datetime.datetime.now()#gets local time
+    currentTime = dt.strftime("%D %H:%M:%S")
+    return '%s.%s##' % (currentTime)
+
+@app.route('/getOperatingTime', methods=['GET'])
+def getOperatingTime():
+    on= timeDB.query.get({"id": 1})
+    off = timeDB.query.get({"id": 2})
+
+    dt = datetime.datetime.now()#gets local time
+    currentDay = dt.weekday()
+    if currentDay == 0:
+        onTime = on.mon
+        offTime = off.mon
+    elif currentDay == 1:
+        onTime = on.tue 
+        offTime = off.tue
+    elif currentDay == 2:
+        onTime = on.wed 
+        offTime = off.wed
+    elif currentDay == 3:
+        onTime = on.thu 
+        offTime = off.thu
+    elif currentDay == 4:
+        onTime = on.fri 
+        offTime = off.fri
+    elif currentDay == 5:
+        onTime = on.sat 
+        offTime = off.sat
+    else:
+        onTime = on.sun
+        offTime = off.sun
+    return 'ON %s OFF %s##' % (onTime, offTime)
 
 @app.route('/get_user', methods=['GET'])
 def post_user():
@@ -43,14 +186,29 @@ def post_user():
     resp.status_code = 200
     return resp
 
-@app.route('/jsonpost_user', methods=['POST'])
-def jsonpost_user():
-    user = User(request.json['username'], request.json['email'])
-    # this add to the database the user
-    db.session.add(user)
-    # this saves this data in the database
+@app.route('/jsonpost_status', methods=['POST'])
+def jsonpost_status():
+    dt = datetime.datetime.now()#gets local time
+    currentTime = dt.strftime("%D %H:%M:%S")
+
+    device = pourDB(request.json['deviceNum'], deviceName, request.json['OnOff'], request.json['last_battVolt'], request.json['last_rcPeriod'], currentTime)
+    
     db.session.commit()
-    resp = jsonify('User added successfully!')
+    resp = jsonify('{}',deviceName)#sends device name back
+    resp.status_code = 200
+    return resp
+
+
+@app.route('/jsonpost_data', methods=['POST'])
+def jsonpost_user():
+    dt = datetime.datetime.now()#gets local time
+    currentTime = dt.strftime("%D %H:%M:%S")
+
+    pourInfo = pourDB(request.json['deviceNum'], request.json['deviceName'], request.json['pourVolume'], request.json['approxTime'],currentTime, request.json['battVolt'], request.json['timerCounter'], request.json['pourTime'], request.json['pulseCount'])
+    
+    db.session.add(pourInfo)# this add to the database the user
+    db.session.commit()
+    resp = jsonify('Successful Transfer')
     resp.status_code = 200
     return resp
     #return redirect('/')
